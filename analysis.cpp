74edd6c69f7bb900c68cdc3e2d525f2c93a2ed5c
Andrea Barbieri <andreabarbieri0104@gmail.com>
2024-11-07 18:31:04 +0100

    rip anal

--- analysis.cpp
+++ analysis.cpp
@@ -14,6 +14,28 @@ TH1F* h9 = ( TH1F*)myFile->Get("h_Invariant_mass_same_sign");
 TH1F* h10 = ( TH1F*)myFile->Get("h_Invariant_mass_opp_sign");
 TH1F* h11 = ( TH1F*)myFile->Get("h_Benchmark");
 
-//TH1F h_opp_same =
+
+h10->Add(h9, -1);
+h8->Add(h8, h7, 1, -1);
+
+
+TCanvas *c1 = new TCanvas("c1", "Canvas", 800, 800);
+h10->Draw("H");
+TCanvas *c2 = new TCanvas("c2", "Canvas", 800, 800);
+h8->Draw("H");
+TCanvas *c3 = new TCanvas("c3", "Canvas", 800, 800);
+h11->Draw("H");
+/*TCanvas *c2 = new TCanvas("c2", "2x2 Canvas", 800, 800);
+  c2->Divide(2, 2);
+  c2->cd(1);
+  h7->Draw("H");
+  c2->cd(2);
+  h8->Draw("H");
+  c2->cd(3);
+  h9->Draw("H");
+  c2->cd(4);
+  h10->Draw("H");
+  c2->Update();*/
+
 
 }
